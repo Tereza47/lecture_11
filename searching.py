ef0df296c58807dd6146162cb5573622a62af74d
Tereza47 <xdraho14@vutbr.cz>
2021-05-13 18:01:44 +0200

    Linear search

--- searching.py
+++ searching.py
@@ -23,10 +23,33 @@ def read_data(file_name, field):
     return seq
 
 
+def linear_search(seqence, number):
+    """
+
+    :param seqence:
+    :param number:
+    :return:
+    """
+
+    index_list = []
+    cetnost = 0
+    seq_len = len(seqence)
+    index = 0
+    while index < seq_len:
+        if seqence[index] == number:
+            index_list.append(index)
+            cetnost += 1
+        index += 1
+
+    dictionary = {'positions': index_list, 'count': cetnost}
+    return dictionary
+
+
 def main():
     file_name = 'sequential.json'
     seq = read_data(file_name, "unordered_numbers")
-    print(seq)
+    dict = linear_search(seq, 0)
+    print(dict)
 
 
 if __name__ == '__main__':
